3e12e1d93be0ab5050727f3b648606237cad0c4d
t-a-n-0 <780976408@qq.com>
2020-08-28 18:59:04 +0800

    调整代码格式

--- DanmakuAnalyser.fs
+++ DanmakuAnalyser.fs
@@ -11,136 +11,118 @@ type DanmakusXml = FSharp.Data.XmlProvider<"""
     <d p="param">example2</d>
 </i>""">
 
-module private utils =
-    let getHexColor color =
-        let str = Convert.ToString (Int32.Parse color, 16)
-        str.ToUpperInvariant ()
-        |> (String.init (6 - str.Length) (fun _ -> "0")
-            |> sprintf "#%s%s")
-
-    let getPlayTime =
-        Double.Parse
-        >> TimeSpan.FromSeconds
-        >> sprintf "%A"
-
-    let unixStampBase = DateTime (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
-
-    let getTime time =
-        unixStampBase + TimeSpan (Int64.Parse time * 1000_0000L)
-        |> sprintf "%A"
-
-    let getPool =
-        function
-        | "0" -> "普通池"
-        | "1" -> "字幕池"
-        | "2" -> "特殊池"
-        |  _  -> "?"
-
-    let getMode =
-        function
-        | "1" | "2" | "3" -> "普通弹幕"
-        | "4" -> "底端弹幕"
-        | "5" -> "顶端弹幕"
-        | "6" -> "逆向弹幕"
-        | "7" -> "精准定位"
-        | "8" | "9" -> "高级弹幕"
-        |  _  -> "?"
-
-    let getXAttribute name value =
-        XAttribute (XName.op_Implicit name, value)
-
 module private uidUtils =
-    let private getCrc32bHash n =
-        (n |> string
-         |> Text.ASCIIEncoding.ASCII.GetBytes
-         |> Force.Crc32.Crc32Algorithm.Compute
-        ).ToString "x"
+    let private getCrc32bHash num =
+        num
+        |> string
+        |> Text.ASCIIEncoding.ASCII.GetBytes
+        |> Force.Crc32.Crc32Algorithm.Compute
+        |> (fun s -> s.ToString "x")
 
     type private FindResult =
-    | Result of max: int64 * int * (int list * int64)[]
-    | DictErr
+        | Result of max: int64 * current: int * newUids: (int list * int64) list
+        | DictErr
 
     /// 1 -> 50001
-    let private findUidsFromDictCache cache basei now total ids =
+    let private findUidsFromDictCache cache basei (now, total) ids =
         let mutable uids = list.Empty
         let mutable hasErr = false
         let mutable now = now
-        cache 
+
+        cache
         |> Array.Parallel.iteri (fun i str ->
             match ids |> Array.tryFindIndex (fun (_, s) -> s = str) with
             | Some idi ->
-                let indexs, ss = ids.[idi]
+                let indexs, id = ids.[idi]
                 let state = basei + int64 i
-                if getCrc32bHash state = ss then
+                if getCrc32bHash state = id then
                     now <- now + 1
-                    sprintf "[%i/%i] %s -> %i（位于 %A）" now total ss state indexs |> Console.WriteLine
                     uids <- (indexs, state) :: uids
+                    sprintf "[%i/%i] %s -> %i（位于 %A）" now total id state indexs 
+                    |> Console.WriteLine
                 elif hasErr then ()
                 else
-                    sprintf "错误：%s -> %i" ss state |> Console.WriteLine
+                    sprintf "错误：%s -> %i" id state 
+                    |> Console.WriteLine
                     hasErr <- true
             | _ -> ()
         )
-        if hasErr 
+
+        if hasErr
         then DictErr
         else
-            let actualLen = 
-                cache 
-                |> Array.Parallel.choose (fun i -> if String.IsNullOrEmpty i then None else Some ())
+            let actualLength =
+                cache
+                |> Array.Parallel.choose 
+                    (fun i -> 
+                        if String.IsNullOrEmpty i 
+                        then None 
+                        else Some ())
                 |> Array.length
                 |> int64
-            Result (basei + actualLen, now, List.toArray uids)
+            Result (basei + actualLength, now, uids)
 
-    let rec private readCache current (state: string[]) (sr: StreamReader) =
-        if sr.Peek () = -1 || current = state.Length
+    let rec private readCache current (state: string[]) (reader: StreamReader) =
+        if reader.Peek () = -1 || current = state.Length
         then state
         else
-            state.[current] <- sr.ReadLine ()
-            readCache (current + 1) state sr
+            state.[current] <- reader.ReadLine ()
+            readCache (current + 1) state reader
 
-    let rec private findUidsFromDict state sr now total ids uids =
+    let rec private findUidsFromDict state reader (now, total) ids uids =
         match ids with
         | [||] -> Result (state + 1L, now, uids)
         | _ ->
-            let cache = readCache 0 (Array.create 5_0000 "") sr
-            match findUidsFromDictCache cache (state + 1L) now total ids with
+            let cache = readCache 0 (Array.create 5_0000 "") reader
+            match findUidsFromDictCache cache (state + 1L) (now, total) ids with
             | DictErr -> DictErr
             | Result (m, n, us) ->
-                if sr.Peek () = -1
-                then Result (m, n, Array.append uids us)
+                if reader.Peek () = -1
+                then Result (m, n, us @ uids)
                 else
-                    let us', _ = Array.unzip us
-                    let ids' = ids |> Array.Parallel.choose (fun ((i, _) as t) -> if us' |> Array.contains i then None else Some t )
-                    findUidsFromDict (m - 1L) sr n total ids' (Array.append uids us)
+                    let us', _ = List.unzip us
+                    let ids' = 
+                        ids 
+                        |> Array.Parallel.choose 
+                            (fun ((i, _) as t) -> 
+                                if us' |> List.contains i 
+                                then None 
+                                else Some t)
+                    findUidsFromDict (m - 1L) reader (n, total) ids' (us @ uids)
 
     /// state 初始值是 1
-    let rec private calUidFromId state id (sw: StreamWriter) =
+    let rec private calUidAndEnlargeDict state id (sw: StreamWriter) =
         let hs =
             [| state .. state + 999L |]
             |> Array.Parallel.map getCrc32bHash
-        hs 
-        |> String.concat "\n" 
+        hs
+        |> String.concat "\n"
         |> sw.WriteLine
         match hs |> Array.tryFindIndex ((=) id) with
-        | None -> calUidFromId (state + 1000L) id sw
+        | None -> calUidAndEnlargeDict (state + 1000L) id sw
         | Some i -> state + int64 i
 
     type private GetResult =
-    | Continue of int list * int64
-    | Break of string[]
+        | Continue of int list * int64
+        | Break of string[]
 
     let private _getUids (cacheFileInfo: FileInfo) ids uids lastMax lastPosition now total =
         match ids with
-        | [||] -> 
+        | [||] ->
             uids
-            |> Array.Parallel.map (fun (indexs, uid) -> 
-                indexs 
-                |> List.toArray 
-                |> Array.Parallel.map (fun i -> i, string uid)
-            ) |> Array.concat
-            |> Array.sortBy (fun (i, _) -> i)
-            |> Array.Parallel.map (fun (_, s) -> s)
-            |> Break 
+            |> List.toArray
+            |> Array.Parallel.map
+                (fun (indexs, uid) ->
+                    indexs
+                    |> List.toArray
+                    |> Array.Parallel.map
+                        (fun i -> i, string uid))
+            |> Array.concat
+            |> Array.sortBy
+                (fun (i, _) -> i)
+            |> Array.Parallel.map
+                (fun (_, s) -> s)
+            |> Break
         | _ ->
             printfn "\n[一]扩充字典中……\n"
             let indexs, ss = ids.[0]
@@ -148,49 +130,99 @@ module private uidUtils =
             let ws = cacheFileInfo.OpenWrite()
             ws.Position <- lastPosition
             let sw = new StreamWriter (ws)
-            let uid = calUidFromId lastMax ss sw
+            let uid = calUidAndEnlargeDict lastMax ss sw
             sw.Dispose ()
 
-            sprintf "[%i/%i] %s -> %i（位于 %A）" now total ss uid indexs |> Console.WriteLine
-            Continue <| (indexs, uid)
+            sprintf "[%i/%i] %s -> %i（位于 %A）" now total ss uid indexs
+            |> Console.WriteLine
+            Continue (indexs, uid)
 
-    let private cachePath = AppDomain.CurrentDomain.BaseDirectory + "/hash_cache"
+    let private dictPath = AppDomain.CurrentDomain.BaseDirectory + "/hash_cache"
 
-    let private getNewIdAndUidArray uids uids' ids = 
-        let uids'', _ = uids' |> Array.unzip
-        let _, ids' = ids |> Array.Parallel.partition (fun (i, _) -> uids'' |> Array.contains i)
-        ids', Array.append uids uids'
+    let private removeSameIndexedIds newUids ids =
+        let indexs, _ = newUids |> List.unzip
+        ids
+        |> Array.Parallel.choose
+            (fun ((i, _) as t) ->
+                if indexs |> List.contains i
+                then None
+                else Some t)
 
-    let rec getUids (lastMax, lastPosition) now total uids ids =
-        let cacheFile = FileInfo (cachePath)
+    let rec getUids (lastReadedMax, lastReadedPosition) (current, total) uids ids =
+        let cacheFile = FileInfo (dictPath)
         if not cacheFile.Exists
-        then cacheFile.CreateText().Dispose()
+        then cacheFile.CreateText().Dispose ()
 
-        let rs = cacheFile.OpenRead()
-        rs.Position <- if lastPosition = 0L then 0L else lastPosition + 1L
-        let reader = new StreamReader (rs)
-        let rrr = findUidsFromDict (lastMax - 1L) reader now total ids [||]
-        let pos = reader.BaseStream.Position
-        reader.Dispose ()
+        let readStream = cacheFile.OpenRead ()
+        readStream.Position <-
+            if lastReadedPosition = 0L
+            then 0L
+            else lastReadedPosition + 1L
+        let streamReader = new StreamReader (readStream)
+        let foundResult = findUidsFromDict (lastReadedMax - 1L) streamReader (current, total) ids []
+        let lastReadedPosition' = streamReader.BaseStream.Position
+        streamReader.Dispose ()
 
-        match rrr with
+        match foundResult with
         | DictErr ->
             cacheFile.Delete ()
-            printfn "[一]字典中包含错误，已将其移除并准备重新生成。"
-            printfn "    可能需要重新计算部分的 Uid 。"
-            getUids (1L, 0L) now total uids ids
+            printfn "
+[一]字典中包含错误，已将其移除并准备重新生成。
+    可能需要重新计算部分的 Uid 。"
+            getUids (1L, 0L) (current, total) uids ids
         | Result (m, n, uids') ->
-            let ids', uids'' = getNewIdAndUidArray uids uids' ids
+            let ids' = removeSameIndexedIds uids' ids
             let n = n + 1
-            match _getUids cacheFile ids' uids'' m pos n total with
+            match _getUids cacheFile ids' (uids' @ uids) m lastReadedPosition' n total with
+            | Continue (indexs, uid) ->
+                let uids' = [ indexs, uid ]
+                let ids' = removeSameIndexedIds uids' ids'
+                getUids (m, lastReadedPosition') (n, total) (uids' @ uids) ids'
             | Break result -> result
-            | Continue (indexs, uid) -> 
-                let ids', uids'' = getNewIdAndUidArray uids'' [| indexs, uid |] ids'
-                getUids (m, pos) n total uids'' ids'
-
-open utils
 
 module DanmakuAnalyser =
+    let private getHexColor color =
+        let str = Convert.ToString (Int32.Parse color, 16)
+        str.ToUpperInvariant ()
+        |> (String.init (6 - str.Length) (fun _ -> "0")
+            |> sprintf "#%s%s")
+
+    let private getPlayTime time =
+        time
+        |>Double.Parse
+        |> TimeSpan.FromSeconds
+        |> string
+
+    let private unixStampBase =
+        DateTime (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+    let private getTime time =
+        let time =
+            time + "0000000"
+            |> Int64.Parse
+            |> TimeSpan
+        unixStampBase + time
+        |> string
+
+    let private getPool =
+        function
+        | "0" -> "普通池"
+        | "1" -> "字幕池"
+        | "2" -> "特殊池"
+        | _ -> "未知"
+
+    let private getMode =
+        function
+        | "1" | "2" | "3" -> "普通弹幕"
+        | "4" -> "底端弹幕"
+        | "5" -> "顶端弹幕"
+        | "6" -> "逆向弹幕"
+        | "7" -> "精准定位"
+        | "8" | "9" -> "高级弹幕"
+        | _ -> "未知"
+
+    let private getXAttribute name value =
+        XAttribute (XName.op_Implicit name, value)
+
     let private getDanmakuData (node: DanmakusXml.D) uid =
         let args = node.P.Split ','
         XElement (
@@ -205,30 +237,41 @@ module DanmakuAnalyser =
             getXAttribute "fontsize"                args.[2]
         )
 
+    let private getUids parseUid (danmakus: DanmakusXml.D[]) =
+        let ids =
+            danmakus
+            |> Array.Parallel.map
+                (fun i -> (i.P.Split ',').[6])
+
+        if parseUid then
+            printfn "开始解析 Uid ……"
+            let indexedIds =
+                ids
+                |> Array.Parallel.mapi
+                    (fun i s -> i, s)
+
+            ids
+            |> Array.distinct
+            |> Array.Parallel.map
+                (fun str ->
+                    let indexs =
+                        indexedIds
+                        |> Array.Parallel.choose
+                            (fun (i, s) ->
+                                if str = s
+                                then Some i
+                                else None)
+                        |> Array.toList
+                    indexs, str)
+            |> (fun r ->
+                    uidUtils.getUids (1L, 0L) (0, r.Length) [] r)
+        else ids
+
     let getDanmakus parseUid (xml: string) =
         let ds = DanmakusXml.Parse xml
 
         printfn "共有%i条弹幕。" ds.Ds.Length
-        let ids =
-            if parseUid then
-                printfn "开始解析 Uid ……"
-                let t =
-                    ds.Ds
-                    |> Array.Parallel.mapi (fun i j -> i, (j.P.Split ',').[6])
-                ds.Ds 
-                |> Array.Parallel.map (fun i -> (i.P.Split ',').[6])
-                |> Array.distinct
-                |> Array.Parallel.map (fun str -> 
-                    let indexs = 
-                        t |> Array.Parallel.choose (fun (i, s) -> 
-                            if str = s 
-                            then Some i
-                            else None
-                        ) |> Array.toList
-                    indexs, str
-                )
-                |> (fun i -> uidUtils.getUids (1L, 0L) 0 i.Length [||] i)
-            else ds.Ds |> Array.Parallel.map (fun i -> (i.P.Split ',').[6])
+        let ids = getUids parseUid ds.Ds
 
         ds.Chatid,
         XDocument (
--- Program.fs
+++ Program.fs
@@ -1,4 +1,5 @@
-﻿open BilibiliDanmakuParser
+﻿module BilibiliDanmakuParser.Main
+
 open System
 open System.IO
 open System.IO.Compression
@@ -10,36 +11,43 @@ let printlnInRed msg =
     printfn "%s" msg
     Console.ForegroundColor <- ConsoleColor.Gray
 
-let rec saveResult n (cid, xdoc: XDocument) =
-    let path = sprintf "%s%cresult_%i_(%i).xml" Environment.CurrentDirectory Path.DirectorySeparatorChar cid n
+let rec saveResult fileIndex (cid, xdoc: XDocument) =
+    let indexText =
+        if fileIndex = 0
+        then String.Empty
+        else sprintf "(%i)" fileIndex
+    let path =
+        sprintf "%s%cresult_%i%s.xml"
+            Environment.CurrentDirectory
+            Path.DirectorySeparatorChar
+            cid
+            indexText
+
     if File.Exists path
-    then saveResult (n + 1) (cid, xdoc)
+    then saveResult (fileIndex + 1) (cid, xdoc)
     else
         let stream = File.Create path
         xdoc.Save (stream, SaveOptions.None)
         path
 
-let fromXml parseUid xml =
+let fromXml needParseUid xml =
     xml
-    |> DanmakuAnalyser.getDanmakus parseUid
+    |> DanmakuAnalyser.getDanmakus needParseUid
     |> saveResult 0
     |> printfn "\n分析完成，已保存至%s。"
 
-let fromCid parseUid cid =
-    printfn "正在获取弹幕列表……"
+let fromCid needParseUid cid =
+    printfn "正在获取该视频的弹幕列表……"
     use client = new HttpClient ()
-    let uri =
-        cid
-        |> sprintf "https://comment.bilibili.com/%i.xml"
-        |> Uri
+    let uri = sprintf "https://comment.bilibili.com/%i.xml" cid
     async {
         let! bytes = client.GetByteArrayAsync uri |> Async.AwaitTask
         use memoryStream  = new MemoryStream (bytes)
         use deflateStream = new DeflateStream (memoryStream, CompressionMode.Decompress)
         use streamReader  = new StreamReader (deflateStream, Text.Encoding.UTF8)
         let! result = streamReader.ReadToEndAsync() |> Async.AwaitTask
-        printfn "获取完毕。\n"
-        fromXml parseUid result
+        printfn "获取完成。\n"
+        fromXml needParseUid result
     } |> Async.RunSynchronously
 
 module FromVideoPart =
@@ -57,120 +65,133 @@ module FromVideoPart =
     }
     """>
 
-    let private showParts (data: PartsJson.Datum seq) =
+    let private printPartInfos (data: PartsJson.Datum[]) =
+        printfn "该稿件共有%i个视频。" data.Length
         for i in data do
             printfn "[%i]%s" i.Page i.Part
 
-    let rec private readPartIndex max =
+    let rec private readSelectedPartIndex (data: PartsJson.Datum[]) =
+        printPartInfos data
         printfn "\n请选择一个视频："
-        let input = Console.ReadLine ()
-        let mutable r = 0
-        if Int32.TryParse (input, &r) && r > 0 && r <= max
-        then r
-        else
-            printlnInRed "输入的内容错误。"
-            readPartIndex max
 
-    let private cli parseUid (uri: string) =
+        let input = Console.ReadLine ()
+        let mutable result = 0
+        if Int32.TryParse (input, &result) && result > 0 && result <= data.Length then
+            printfn "\n已选择 p%i ：%s 。" result data.[result - 1].Part
+            result
+        else
+            data.Length
+            |> sprintf "输入的内容 i 应在 [1, %i] 内。\n"
+            |> printlnInRed
+            readSelectedPartIndex data
+
+    let private cli needParseUid (uri: string) =
         use client = new HttpClient ()
         async {
-            let! str = client.GetStringAsync uri |> Async.AwaitTask
-            let json = PartsJson.Parse str
-            if json.Code = 0 then
-                printfn "该稿件共有%i个视频。" json.Data.Length
-                showParts json.Data
-                let selectedPart =
-                    if json.Data.Length = 1
+            let! partsInfo = client.GetStringAsync uri |> Async.AwaitTask
+            let parts = PartsJson.Parse partsInfo
+
+            if parts.Code = 0 then
+                let selected =
+                    if parts.Data.Length = 1
                     then 1
-                    else readPartIndex json.Data.Length
-                printfn "\n已选择 p%i ：%s 。" selectedPart json.Data.[selectedPart - 1].Part
-                fromCid parseUid json.Data.[selectedPart - 1].Cid
-            else printlnInRed <| sprintf "服务器返回错误代码%i：%s。" json.Code json.Message
+                    else readSelectedPartIndex parts.Data
+                fromCid needParseUid
+                    parts.Data.[selected - 1].Cid
+
+            else sprintf "服务器返回 { %i }：%s。"
+                    parts.Code
+                    parts.Message
+                 |> printlnInRed
         } |> Async.RunSynchronously
 
-    let cliAvid parseUid =
-        Int64.Parse
-        >> sprintf "https://api.bilibili.com/x/player/pagelist?aid=%i"
-        >> cli parseUid
+    let fromAvid needParseUid aid =
+        aid
+        |> Int64.Parse
+        |> sprintf "https://api.bilibili.com/x/player/pagelist?aid=%i"
+        |> cli needParseUid
 
-    let cliBvid parseUid =
-        sprintf "https://api.bilibili.com/x/player/pagelist?bvid=%s"
-        >> cli parseUid
-
-let showHelp () =
-    printfn "获取Bilibili的弹幕数据。"
-    printfn ""
-    printfn "用法："
-    printfn "    BilibiliDanmakuParser 命令 参数"
-    printfn "命令及参数说明："
-    printfn "    -av, --FromAVId"
-    printfn "    -avu, --FromAVIdu"
-    printfn "        参数：AV号"
-    printfn "    -bv, --FromBVId"
-    printfn "    -bvu, --FromBVIdu"
-    printfn "        参数：BV号（包含前缀“BV”）"
-    printfn "        获取特定稿件下某视频的弹幕列表。"
-    printfn "    -c, --FromCId"
-    printfn "    -cu, --FromCIdu"
-    printfn "        参数：某视频的编号（即“cid”）"
-    printfn "        获取特定BV号下的弹幕列表。"
-    printfn "    -f, --FromDanmakuFile"
-    printfn "    -fu, --FromDanmakuFileu"
-    printfn "        参数：以XML格式保存的弹幕文件路径"
-    printfn "        分析指定的弹幕文件。"
-    printfn "    以“u”结尾的命令生成的文件会解密 Uid，但由于程序用的是暴力枚举 Uid ，"
-    printfn "运行速度很慢，同时会占用部分磁盘空间（大约 6GiB）。"
+    let fromBvid needParseUid bvid =
+        bvid
+        |> sprintf "https://api.bilibili.com/x/player/pagelist?bvid=%s"
+        |> cli needParseUid
 
 let validCmd =
     [
-        "-av" ; "--FromAVId"
-        "-avu"; "--FromAVIdu"
-        "-bv" ; "--FromBVId"
-        "-bvu"; "--FromBVIdu"
-        "-c"  ; "--FromCId"
-        "-cu" ; "--FromCIdu"
-        "-f"  ; "--FromDanmakuFile"
-        "-fu" ; "--FromDanmakuFileu"
+        "-av" ; "--FromAVId"       ; "-avu"; "--FromAVIdu"
+        "-bv" ; "--FromBVId"       ; "-bvu"; "--FromBVIdu"
+        "-c"  ; "--FromCId"        ; "-cu" ; "--FromCIdu"
+        "-f"  ; "--FromDanmakuFile"; "-fu" ; "--FromDanmakuFileu"
     ]
 
 [<EntryPoint>]
 let main argv =
     try
-        let sw = Diagnostics.Stopwatch ()
-        Console.CancelKeyPress.Add(fun _ ->
-            printfn "\n程序中止。"
-        )
-        if argv.Length <> 0 then
+        Console.CancelKeyPress.Add
+            (fun _ -> printfn "\n程序中止。")
+        if argv.Length > 0 then
             argv
-            |> String.concat " "
-            |> printfn "当前命令：%s\n"
+            |> Array.fold
+                (fun state s ->
+                    sprintf "%s \"%s\"" state s)
+                "当前命令："
+            |> printfn "%s\n"
+        let stopwatch = Diagnostics.Stopwatch ()
+        stopwatch.Start ()
 
-        sw.Start ()
         match argv with
         | [| cmd; arg |] when validCmd |> List.contains cmd ->
-            (cmd.[cmd.Length - 1] = 'u', arg) ||>
+            let parseUid = cmd.[cmd.Length - 1] = 'u'
             match cmd with
-            | "-av" | "--FromAVId" | "-avu" | "--FromAVIdu" -> FromVideoPart.cliAvid
-            | "-bv" | "--FromBVId" | "-bvu" | "--FromBVIdu" -> FromVideoPart.cliBvid
-            | "-c"  | "--FromCId"  | "-cu"  | "--FromCIdu"  -> 
-                fun parseUid cid ->
-                    Int64.Parse cid
-                    |> fromCid parseUid
-
+            | "-av" | "--FromAVId" | "-avu" | "--FromAVIdu" ->
+                FromVideoPart.fromAvid parseUid arg
+            | "-bv" | "--FromBVId" | "-bvu" | "--FromBVIdu" ->
+                FromVideoPart.fromBvid parseUid arg
+            | "-c" | "--FromCId" | "-cu" | "--FromCIdu" ->
+                arg
+                |> Int64.Parse
+                |> fromCid parseUid
             | "-f"  | "--FromDanmakuFile" | "-fu" | "--FromDanmakuFileu" ->
-                fun parseUid filePath ->
-                    File.ReadAllText filePath
-                    |> fromXml parseUid
-
-            | _ -> fun _ _ -> ()
-
+                arg
+                |> File.ReadAllText
+                |> fromXml parseUid
+            | _ -> ()
         | _ ->
-            if argv.Length <> 0 then
-                printlnInRed "命令不正确。"
-            showHelp ()
-        sw.Stop ()
-        printfn "\n本次运行耗时 %A 。" sw.Elapsed
+            if argv.Length > 0
+            then printlnInRed "命令不正确。"
+            printfn "
+获取Bilibili的弹幕数据。
+
+用法：
+    BilibiliDanmakuParser 命令 参数
+
+命令及参数说明：
+    -av, --FromAVId
+    -avu, --FromAVIdu
+        参数：AV号
+    -bv, --FromBVId
+    -bvu, --FromBVIdu
+        参数：BV号（包含前缀“BV”）
+        获取特定稿件下某视频的弹幕列表。
+
+    -c, --FromCId
+    -cu, --FromCIdu
+        参数：某视频的编号（即“cid”）
+        获取特定BV号下的弹幕列表。
+
+    -f, --FromDanmakuFile
+    -fu, --FromDanmakuFileu
+        参数：以XML格式保存的弹幕文件路径
+        分析指定的弹幕文件。
+
+        以“u”结尾的命令生成的文件会解密 Uid，但由于程序是通过
+    暴力枚举来计算 Uid 的，运行速度会非常很慢，同时会占用部分
+    磁盘空间来储存解密字典（大约 6GiB）。"
+
+        stopwatch.Stop ()
+        printfn "\n本次运行耗时 %A 。" stopwatch.Elapsed
         0
     with ex ->
-        printlnInRed <| sprintf "发生异常。\n%A" ex
+        sprintf "发生异常。\n%A" ex
+        |> printlnInRed
         ex.HResult
